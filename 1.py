d253438f957f0d6915b15e67cfb0c03bfb74c238
Chad Bramwell <chadbramwell@gmail.com>
2023-12-24 13:35:38 -0800

    1.1

---
+++ 1.py
@@ -0,0 +1,20 @@
+lines = open("1.txt", "r").readlines()
+
+sum = 0
+for line in lines:
+    value = -1
+    last = -1
+    for rune in line:
+        if rune.isdigit():
+            if value == -1:
+                value = int(rune)
+            else:
+                last = int(rune)
+    #print(first, last)
+    if last == -1:
+        value = value * 10 + value
+    else:
+        value = value * 10 + last
+    print(value)
+    sum += value
+print(sum)
